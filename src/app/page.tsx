1cee14d6e82caccec63e4e4c939e818e4bf2a7ab
mylife-as-miles <siscomilesinfo@gmail.com>
2025-07-16 20:53:25 -0700

    refactor: simplify imports in page.tsx by removing unused dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,4 @@
-import ProductListSec from "@/components/common/ProductListSec";
-import Brands from "@/components/homepage/Brands";
-import DressStyle from "@/components/homepage/DressStyle";
-import Header from "@/components/homepage/Header";
-import Reviews from "@/components/homepage/Reviews";
-import { Product } from "@/types/product.types";
-import { Review } from "@/types/review.types";
-
+import React from "react";
 import ProductListSec from "@/components/common/ProductListSec";
 import Brands from "@/components/homepage/Brands";
 import DressStyle from "@/components/homepage/DressStyle";
